8b5520cdb73688c11f7f977108a5c85129502825
Vanvi Rao <vanvirao@gmail.com>
2026-09-10 15:14:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,20 +3,22 @@ const mysql = require("mysql2");
 const express = require("express");
 const cors = require("cors");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-db.connect((err) => {
+db.query("SELECT 1", (err) => {
     if (err) {
         console.error("Database connection failed:", err.message);
         return;
     }
-
     console.log("Connected to MySQL!");
 });
 
